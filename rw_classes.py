9c0c4621005112d90c5c363a8014e638e4f1fdd5
masavujovic <vujovic.masa1@gmail.com>
2020-01-10 16:17:41 +0100

    Update rw_classes.py

--- rw_classes.py
+++ rw_classes.py
@@ -13,18 +13,23 @@ class RescorlaWagnerNetwork:
 		self.theta = np.zeros([self.number_of_categories, self.exemplars.shape[1]])
 
 	def trial(self):
-		# on a single trial, choose a category with probability p (HF 75%, LF 25% for example)
+		"""Choose stimuli for single learning trial.
+		Choose a category choice with probability p,
+		and an exemplar from that category.
+		Choose a random category label.
+    		"""
 		choice = np.random.choice(np.arange(0, self.exemplars.shape[0]), p=self.probabilities)
-		# then choose an exemplar from that category
 		exemplar = self.exemplars[choice, :]
-		# then choose a category label at random
 		label = random.randint(0, self.number_of_categories - 1)
 		return choice, label
 
 	def outcome_present(self, choice, label):
-		# if the randomly chosen label (argument "label") matches the actual category 
-		# of the exemplar (argument "exemplar") --> outcome = 1 (lambda)
-		# this means outcome was present (remember that at each trial weights get updated for all outcomes, present and otherwise)
+		"""Determine if category matches label.
+		
+		Keyword arguments:
+		choice -- randomly chosen category
+		label -- randomly chosen label
+		"""
 		if self.network_type=='fl':
 			outcome = 1. if label==self.categories[choice] else 0.
 		else:
@@ -32,20 +37,25 @@ class RescorlaWagnerNetwork:
 		return outcome
 
 	def do_trial(self, alpha, beta):
-		# randomly chose a category and a label
+		"""Run a single learning trial
+		
+		For a randomly chosen category and label, determine
+		whether they match.
+		
+		Sum the weights for all the features associated with the label.
+		Update weights via gradient descent.
+		
+		Keyword arguments:
+		alpha -- hyperparameter (learning rate)
+		beta -- hyperparameter (cue saliency)
+		"""
 		choice, label = self.trial()
-		# determine if they match (if outcome was "present")
 		outcome = self.outcome_present(choice, label)
 		
-		# if netowrk is fl (feature-label) -- suffixing
  		if self.network_type=='fl':
- 			# v_total: sum all the weights for all the features for a given label (dot product)
 			v_total = np.dot(self.theta[label,:], np.transpose(self.exemplars[choice,:]))
-			# update theta (weight vector) = multiply alpha, beta, lambda (outcome), v_total, and the features vector
-			# which is self.exemplars[choice,:] -- each exemplar is a vector of 0 and 1 (0 - exemplar does not have that feature; 1 - exemplar has that feature)
 			self.theta[label,:] += alpha*beta*(outcome - v_total)*self.exemplars[choice,:]
 		else:
-			# same but swapped around (prefix)
 			cat_of_choice = self.categories[choice]
 			cues = np.zeros([self.number_of_categories])
 			cues[cat_of_choice] = 1
@@ -53,38 +63,41 @@ class RescorlaWagnerNetwork:
 			self.theta[cat_of_choice,:] += alpha*beta*(outcome - v_total)
 
 def simulation():
+	"""Generate a learning set 
+	
+	learning_set is a matrix with dimensions correspondiing to 
+	the number of exemplars and the number of features
+
+	"""
 	num_exemplars = 4
 	num_features = 6
-	# cat is a matrix with dimensions corresponding to number of exemplars
-	# and number of features 
-	cat = np.zeros([num_exemplars, num_features])
 	
-	# e.g. cat[0,0] means that exemplar 1 has feature 1 present
-	cat[0,0] = 1
-	cat[0,2] = 1
-	cat[1,1] = 1
-	cat[1,3] = 1
-	cat[2,1] = 1
-	cat[2,4] = 1
-	cat[3,0] = 1
-	cat[3,5] = 1
+	learning_set = np.zeros([num_exemplars, num_features])
+	
+	# e.g. learning_set[0,0] = 1 means that exemplar 1 has feature 1 present
+	learning_set[0,0] = 1
+	learning_set[0,2] = 1
+	learning_set[1,1] = 1
+	learning_set[1,3] = 1
+	learning_set[2,1] = 1
+	learning_set[2,4] = 1
+	learning_set[3,0] = 1
+	learning_set[3,5] = 1
 
-	# as many as there are examplars 
 	categories = np.array([0,0,1,1])
 	
-	# probs of each exemplar appearing (type frequency manipulation) divided by number of categories
+	# probability of each exemplar being chosen, divided by number of categories
 	probabilities = np.array([0.75, 0.25, 0.75, 0.25])/2.
 
 	number_of_categories = 2
 
 	# this is where we call the function (all the arguments explained above; the last string means suffix or prefix) 
-	ntwrk = RescorlaWagnerNetwork(cat, probabilities, categories, number_of_categories, 'lf')
+	ntwrk = RescorlaWagnerNetwork(learning_set, probabilities, categories, number_of_categories, 'lf')
 	
 	n = 0
 
 	with open('thetas.csv', 'w') as csvfile: 
 		writer = csv.writer(csvfile, delimiter = ' ')
-		# n total number of trials
 		while n < 5000:
 			ntwrk.do_trial(0.1, 0.1)
 			n += 1
